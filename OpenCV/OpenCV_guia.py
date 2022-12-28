3d59c91e255dbc9c9ab1d2dff28973dc0cf3ec0f
Jesus <jgomcano@emeal.nttdata.com>
2022-12-28 13:34:30 +0100

    Añadiendo información OpenCV

--- OpenCV/OpenCV_guia.py
+++ OpenCV/OpenCV_guia.py
@@ -5,14 +5,12 @@ import matplotlib.pyplot as plt
 # %matplotlib inline # en cuadernos jupiter para poder mostrar directamente las imagenes del cuaderno
 from IPython.display import Image  # nos permitirá mostrar y renderizar imágenes directamente en el cuaderno.
 
-# ***********************************
-# ***** Leer imágenes usando OpenCV
-# ***********************************
+# *****************************************************************************************************************
+# ***** Leer, imprimir, split, mergear, Conversión a diferentes espacios de color y guardar imágenes usando OpenCV
+# *****************************************************************************************************************
 Image(filename='checkerboard_18x18.png')  # mostrar 18x18 pixel image (en notebook) solo .
 
-# **********************************************
 # ***** Leer e imprimir imágenes usando OpenCV
-# **********************************************
 '''
 OpenCV permite leer diferentes tipos de imágenes (JPG, PNG, etc). Puede cargar imágenes en escala de grises, imágenes 
 en color o también puede cargar imágenes con canal alfa. Utiliza la función cv2.imread() que tiene la siguiente 
@@ -39,7 +37,7 @@ Documentación OpenCV
 ImreadModes: https://docs.opencv.org/4.5.1/d8/d6a/group__imgcodecs__flags.html#ga61d9b0126a3e57d9277ac48327799c80
 '''
 
-# **** leer la imagen en escala de grises e introducirlo en la variable img
+# leer la imagen en escala de grises e introducirlo en la variable img
 img = cv2.imread('checkerboard_18x18.png', 0)  # cargamos la imagen con imread, o en escala de grises
 # Lo que se carga en mi memoria es una matriz 2D de Numpy que representa la imagen.
 
@@ -130,9 +128,7 @@ plt.imshow(coke_img_channels_reversed)
 plt.show()
 
 
-# ***********************************
 # ***** Split y mege imágenes usando OpenCV
-# ***********************************
 '''División y fusión de canales de color
 - cv2.split() Divide una matriz multicanal en varias matrices de un solo canal.
 - cv2.merge() Combina varias matrices para crear una única matriz multicanal. Todas las matrices de entrada deben tener 
@@ -171,9 +167,7 @@ plt.subplot(144); plt.imshow(imgMerged[:, :, ::-1]); plt.title("Merged Output");
 plt.show()
 
 
-# ************************************************
 # ***** Conversión a diferentes espacios de color
-# ************************************************
 '''
 - cv2.cvtColor() Convierte una imagen de un espacio de color a otro. La función convierte una imagen de entrada de un 
                 espacio de color a otro. En caso de una transformación del espacio de color RGB, el orden de los canales 
@@ -194,7 +188,7 @@ Documentación OpenCV
 cv2.cvtColor: https://docs.opencv.org/3.4/d8/d01/group__imgproc__color__conversions.html#ga397ae87e1288a81d2363b61574eb8cab 
 ColorConversionCodes: https://docs.opencv.org/4.5.1/d8/d01/group__imgproc__color__conversions.html#ga4e0972be5de079fed4e3a10e24ef5ef0'''
 
-# **** Cambiando BGR a RGB
+# Cambiando BGR a RGB
 # OpenCV almacena los canales de color en un orden diferente al de la mayoría de las otras aplicaciones (BGR vs RGB).
 img_NZ_rgb = cv2.cvtColor(img_NZ_bgr, cv2.COLOR_BGR2RGB)  # estamos pasando la imagen y un flag que indica la conversión
 plt.imshow(img_NZ_rgb)  # con el cambio Simplemente estamos mostrando la imagen original.
@@ -248,9 +242,7 @@ plt.subplot(144);plt.imshow(img_NZ_rgb);plt.title("Modified");
 plt.show()
 
 
-# **********************************
 # ***** Guardar imagen cv2.imwrite()
-# **********************************
 '''
 Guardar la imagen es tan trivial como leer una imagen en OpenCV. Usamos la función cv2.imwrite() con dos argumentos. El 
 primero es el nombre del archivo, el segundo argumento es el objeto de la imagen.
@@ -282,9 +274,8 @@ img_NZ_gry = cv2.imread("New_Zealand_Lake_SAVED.png", cv2.IMREAD_GRAYSCALE)
 print("img_NZ_gry shape is: ", img_NZ_gry.shape)  # img_NZ_gry shape is:  (600, 840)
 
 
-# *************************************************
 # ***** mostrar la imagen con matploit o con opencv
-# *************************************************
+
 window1 = cv2.namedWindow("w1")  # creamos una ventana
 cv2.imshow('image', img_NZ_bgr, )  # llamamos al show de OpenCV, OJO como es el de Open cv se guarda y muestra en BGR
 cv2.waitKey(0)  # pulsar una tecla para cerrar la imagen OpenCV si 0, si ponemos numeros seran los segundos de espera
@@ -297,9 +288,226 @@ cv2.waitKey(0)  # pulsar una tecla para cerrar la imagen OpenCV si 0, si ponemos
 
 cv2.destroyWindow(window1)  # destruimos la ventana creada
 
+# *************************************
+# ***** Manipulación básica de imágenes
+# *************************************
 
-# *******************************
-# ***** Basic Image Manipulations
-# *******************************
+# Cargamos imagen original de pruebas en escala de grises
+cb_img = cv2.imread("checkerboard_18x18.png", 0)
 
-#
+# Establezca el mapa de colores en escala de grises para una representación adecuada.
+plt.imshow(cb_img, cmap='gray')
+plt.show()
+print(cb_img)
+'''
+[[  0   0   0   0   0   0 255 255 255 255 255 255   0   0   0   0   0   0]
+ [  0   0   0   0   0   0 255 255 255 255 255 255   0   0   0   0   0   0]
+ [  0   0   0   0   0   0 255 255 255 255 255 255   0   0   0   0   0   0]
+ [  0   0   0   0   0   0 255 255 255 255 255 255   0   0   0   0   0   0]
+ [  0   0   0   0   0   0 255 255 255 255 255 255   0   0   0   0   0   0]
+ [  0   0   0   0   0   0 255 255 255 255 255 255   0   0   0   0   0   0]
+ [255 255 255 255 255 255   0   0   0   0   0   0 255 255 255 255 255 255]
+ [255 255 255 255 255 255   0   0   0   0   0   0 255 255 255 255 255 255]
+ [255 255 255 255 255 255   0   0   0   0   0   0 255 255 255 255 255 255]
+ [255 255 255 255 255 255   0   0   0   0   0   0 255 255 255 255 255 255]
+ [255 255 255 255 255 255   0   0   0   0   0   0 255 255 255 255 255 255]
+ [255 255 255 255 255 255   0   0   0   0   0   0 255 255 255 255 255 255]
+ [  0   0   0   0   0   0 255 255 255 255 255 255   0   0   0   0   0   0]
+ [  0   0   0   0   0   0 255 255 255 255 255 255   0   0   0   0   0   0]
+ [  0   0   0   0   0   0 255 255 255 255 255 255   0   0   0   0   0   0]
+ [  0   0   0   0   0   0 255 255 255 255 255 255   0   0   0   0   0   0]
+ [  0   0   0   0   0   0 255 255 255 255 255 255   0   0   0   0   0   0]
+ [  0   0   0   0   0   0 255 255 255 255 255 255   0   0   0   0   0   0]]'''
+
+# ****** Acceso a píxeles individuales
+'''
+Veamos cómo acceder a un píxel en la imagen. Para acceder a cualquier píxel en una matriz numpy, debe usar notación de 
+matriz:
+- matrix[r,c], donde r es el número de fila y c es el número de columna. la matriz está indexada en 0.
+Por ejemplo, si desea acceder al primer píxel, debe especificar matrix[0,0]. Veamos con algunos ejemplos. 
+Imprimiremos un píxel negro desde la parte superior izquierda y un píxel blanco desde la parte superior central.'''
+
+# Imprime el primer pixel del primer cuadro negro
+print(cb_img[0, 0])  # 0
+# imprima el primer píxel blanco a la derecha del primer cuadro negro
+print(cb_img[0, 6])  # 255
+
+# ****** Modificando los píxeles de las imágenes
+
+cb_img_copy = cb_img.copy()
+# cb_img_copy[2, 2] = 200
+# cb_img_copy[2, 3] = 200
+# cb_img_copy[3, 2] = 200
+# cb_img_copy[3, 3] = 200
+cb_img_copy[2:4, 2:4] = 200  # Lo mismo que lo de antes de una
+
+plt.imshow(cb_img_copy, cmap='gray')
+plt.show()
+print(cb_img_copy)
+
+'''[[  0   0   0   0   0   0 255 255 255 255 255 255   0   0   0   0   0   0]
+ [  0   0   0   0   0   0 255 255 255 255 255 255   0   0   0   0   0   0]
+ [  0   0 200 200   0   0 255 255 255 255 255 255   0   0   0   0   0   0]
+ [  0   0 200 200   0   0 255 255 255 255 255 255   0   0   0   0   0   0]
+ [  0   0   0   0   0   0 255 255 255 255 255 255   0   0   0   0   0   0]
+ [  0   0   0   0   0   0 255 255 255 255 255 255   0   0   0   0   0   0]
+ [255 255 255 255 255 255   0   0   0   0   0   0 255 255 255 255 255 255]
+ [255 255 255 255 255 255   0   0   0   0   0   0 255 255 255 255 255 255]
+ [255 255 255 255 255 255   0   0   0   0   0   0 255 255 255 255 255 255]
+ [255 255 255 255 255 255   0   0   0   0   0   0 255 255 255 255 255 255]
+ [255 255 255 255 255 255   0   0   0   0   0   0 255 255 255 255 255 255]
+ [255 255 255 255 255 255   0   0   0   0   0   0 255 255 255 255 255 255]
+ [  0   0   0   0   0   0 255 255 255 255 255 255   0   0   0   0   0   0]
+ [  0   0   0   0   0   0 255 255 255 255 255 255   0   0   0   0   0   0]
+ [  0   0   0   0   0   0 255 255 255 255 255 255   0   0   0   0   0   0]
+ [  0   0   0   0   0   0 255 255 255 255 255 255   0   0   0   0   0   0]
+ [  0   0   0   0   0   0 255 255 255 255 255 255   0   0   0   0   0   0]
+ [  0   0   0   0   0   0 255 255 255 255 255 255   0   0   0   0   0   0]]
+'''
+
+# ***** Recortar imágenes
+''' Recortar una imagen se logra simplemente seleccionando una región específica (píxel) de la imagen.
+Es simplemente indexar una imagen existente y extraer la región que le interesa.'''
+img_NZ_bgr = cv2.imread("New_Zealand_Boat.jpg", cv2.IMREAD_COLOR)  # cargamos una imagen a color
+img_NZ_rgb = img_NZ_bgr[:, :, ::-1]  # Invertimos el último color
+plt.imshow(img_NZ_rgb)  # Mostramos la imagen
+plt.show()  # para que se muestre
+
+# Recortar la región media de la imagen
+cropped_region = img_NZ_rgb[200:400, 300:600]
+plt.imshow(cropped_region)
+plt.show()
+
+# ***** Cambiar el tamaño de las imágenes
+'''La función de cambio de tamaño cambia el tamaño de la imagen src hacia abajo o hacia arriba hasta el tamaño 
+especificado. El tamaño y el tipo se derivan de src,dsize,fx y fy.
+sintaxis:
+
+ - dst = resize( src, dsize[, dst[, fx[, fy[, interpolation]]]] )
+
+El primero es la imagen de origen, y el segundo argumento requerido es el tamaño de salida deseado de la imagen.
+Y luego hay varios argumentos opcionales  Fx e Fy,  los cuáles son sus factores de escala, que vamos a demostrar a 
+continuación. luego está el método de interpolación ( hay varios a seleccionar ), Por ejemplo, cuando aumenta el tamaño 
+de una imagen, tiene que inventar nuevos píxeles y, por lo tanto, hay una interpolación que se requiere para hacer eso. 
+
+La función tiene 2 argumentos requeridos:
+
+- src: imagen de entrada
+- dsize: tamaño de la imagen de salida
+Los argumentos opcionales que se utilizan a menudo incluyen:
+- fx: Factor de escala a lo largo del eje horizontal; cuando es igual a 0, se calcula como (𝚍𝚘𝚞𝚋𝚕𝚎)𝚍𝚜𝚒𝚣𝚎.𝚠𝚒𝚍𝚝𝚑/𝚜𝚛𝚌.𝚌𝚘𝚕𝚜
+- fy: Factor de escala a lo largo del eje vertical; cuando es igual a 0, se calcula como (𝚍𝚘𝚞𝚋𝚕𝚎)𝚍𝚜𝚒𝚣𝚎.𝚑𝚎𝚒𝚐𝚑𝚝/𝚜𝚛𝚌.𝚛𝚘𝚠𝚜
+
+La imagen de salida tiene el tamaño dsize (cuando no es cero) o el tamaño calculado a partir de src.size(), fx y fy; el 
+tipo de dst es el mismo que el de src.
+
+Documentación OpenCV
+ https://docs.opencv.org/4.5.0/da/d54/group__imgproc__transform.html#ga47a974309e9102f5f08231edc7e7529d
+'''
+
+# método 1: Especificación del factor de escala usando fx y fy
+resized_cropped_region_2x = cv2.resize(cropped_region, None, fx=2, fy=2)
+'''imagen, tamaño de salida ( al usar escala esta bien ponerlo a None factores de escala fx y tener Y.En este ejemplo, 
+sólo vamos a establecerlos en dos Así que vamos a duplicar el tamaño de esta región recortada.Ahora tiene cuatrocientos 
+píxeles de alto y 600 píxeles de ancho.'''
+plt.imshow(resized_cropped_region_2x)
+plt.show()
+
+# método 2: Especificación del tamaño exacto de la imagen de salida
+desired_width = 100
+desired_height = 200
+dim = (desired_width, desired_height)
+'''
+vamos a establecer un ancho y alto específicos para la imagen. en este caso, cien y doscientos respectivamente , y vamos
+a crear este vector bidimensional indicando ambas dimensiones y lo usamos como segundo argumento para la función de 
+cambio de tamaño y mostramos la región recortada redimensionada. la imagen se ha distorsionado ahora porque no 
+mantuvimos la relación de aspecto original.'''
+# Cambiar el tamaño de la imagen de fondo al mismo tamaño que la imagen del logotipo
+resized_cropped_region = cv2.resize(cropped_region, dsize=dim, interpolation=cv2.INTER_AREA)
+plt.imshow(resized_cropped_region)
+plt.show()
+
+# Cambiar el tamaño manteniendo la relación de aspecto
+# método 2: usando 'dsize'
+desired_width = 100
+''' ahora vamos a comenzar especificando un ancho de 100 y luego calcularemos la altura deseada asociada manteniendo 
+la relación de aspecto. Así que aquí estamos creando esta proporción del ancho deseado al ancho original de la imagen 
+y luego usando ese factor para derivar la altura deseada aquí. cuando pasamos esa dimensión revisada a la función de 
+cambio de tamaño, obtenemos una imagen de cien píxeles de ancho y la cantidad adecuada de alto para mantener la 
+relación adecuada, que resulta ser de unos sesenta y siete píxeles.
+
+------------------------------------------------------------
+para saber el ancho y el alto  función shape() (dimensiones) 
+------------------------------------------------------------
+s dimensiones de una imagen dada, como la altura de la imagen, el ancho de la imagen y la cantidad de canales en la 
+imagen, se denominan shape (forma) de la imagen y  se almacena en numpy.ndarray.
+La función shape() puede proporcionar la dimensión de una imagen dada y almacena cada una de las dimensiones de la 
+imagen, como la altura de la imagen, el ancho de la imagen y la cantidad de canales en la imagen en diferentes índices.
+
+La altura de la imagen se almacena en el índice 0.
+El ancho de la imagen se almacena en el índice 1.
+El número de canales en la imagen se almacena en el índice 2.
+
+Ejemplo:
+
+dimensions = input_image.shape
+height = input_image.shape[0] 
+width = input_image.shape[1] 
+number_of_channels = input_image.shape[2]
+
+- input_image: representa la imagen cuyas dimensiones se van a encontrar.
+- dimensions: representan las dimensiones de la imagen.
+- height: representa la altura de la imagen de entrada.
+- width: representa el ancho de la imagen de entrada.
+- number_of_channels: representa el número de canales en la imagen.
+
+La relación de aspecto o ratio de una imagen es la proporción entre el ancho y la altura de la imagen. Se calcula
+dividiendo la anchura entre la altura, y se expresa normalmente con dos números separados por dos puntos. Por ejemplo 
+3:2, significa que por cada tres unidades a lo largo hay dos unidades a lo alto
+'''
+print(cropped_region.shape[1])
+aspect_ratio = desired_width / cropped_region.shape[1]  # calculamos el radio de aspecto
+desired_height = int(cropped_region.shape[0] * aspect_ratio)  # calculamos la nueva altura
+dim = (desired_width, desired_height)
+
+resized_cropped_region = cv2.resize(cropped_region, dsize=dim, interpolation=cv2.INTER_AREA)  # Cambiar el tamaño de img
+plt.imshow(resized_cropped_region)
+plt.imshow(resized_cropped_region)
+
+# Ahora, salvemos la imagen redimensionada (recortada)
+# cambiamos el orden del canal
+resized_cropped_region_2x = resized_cropped_region_2x[:, :, ::-1]
+
+# Save resized image to disk
+cv2.imwrite("resized_cropped_region_2x.png", resized_cropped_region_2x)
+
+# ****** Voltear imágenes¶
+''' Puedes voltearlo horizontalmente, verticalmente o en ambas direcciones
+La función flip voltea la matriz en una de tres formas diferentes (los índices de fila y columna están basados en 0):
+
+Sintaxis de la función
+dst = cv.flip( src, flipCode )
+* dst: matriz de salida del mismo tamaño y tipo que src.
+
+La función tiene 2 argumentos requeridos:
+- src: imagen de entrada
+- flipCode: un flag para especificar cómo voltear la matriz; 
+    - 0 significa girar alrededor del eje x, o voltearlo verticalmente ( boca abajo)
+    - un valor positivo (por ejemplo, 1) significa girar alrededor del eje y, o voltearlo horizontalmente  (espejo)
+    - Un valor negativo (por ejemplo, -1) significa girar alrededor de ambos ejes.
+Documentación OpenCV
+flip: https://docs.opencv.org/4.5.0/d2/de8/group__core__array.html#gaca7be533e3dac7feb70fc60635adf441
+'''
+
+img_NZ_rgb_flipped_horz = cv2.flip(img_NZ_rgb, 1)
+img_NZ_rgb_flipped_vert = cv2.flip(img_NZ_rgb, 0)
+img_NZ_rgb_flipped_both = cv2.flip(img_NZ_rgb, -1)
+
+# mostramos las imágenes
+# mostramos las imágenes
+plt.figure(figsize=[18, 5])
+plt.subplot(141);plt.imshow(img_NZ_rgb_flipped_horz);plt.title("Horizontal Flip");
+plt.subplot(142);plt.imshow(img_NZ_rgb_flipped_vert);plt.title("Vertical Flip");
+plt.subplot(143);plt.imshow(img_NZ_rgb_flipped_both);plt.title("Both Flipped");
+plt.subplot(144);plt.imshow(img_NZ_rgb);plt.title("Original");
+plt.show()
