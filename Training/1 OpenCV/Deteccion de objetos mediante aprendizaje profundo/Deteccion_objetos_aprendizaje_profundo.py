a0ca5e05b49764e4b3987c3da5efed7fe1491ee5
Jesus <jgomcano@emeal.nttdata.com>
2023-01-19 10:48:03 +0100

    Reorganización

---
+++ Training/1 OpenCV/Deteccion de objetos mediante aprendizaje profundo/Deteccion_objetos_aprendizaje_profundo.py
@@ -0,0 +1,174 @@
+# ********************************************************
+# ***** Detección de Objetos mediante aprendizaje profundo
+# ********************************************************
+# 1.Arquitectura: Multi-Box (SSD) basado en Mobilenet
+# 2.Marco: Tensorflow
+'''
+SSD significa detección de caja múltiple de un solo disparo. "un solo disparo" se refiere a que vamos a hacer un único
+pase hacia adelante por la red neuronal para realizar inferencias y, sin embargo, detectar múltiples objetos dentro de
+una imagen. Al igual que otros tipos de redes, los modelos SSD se pueden entrenar con diferentes estructuras troncales
+arquitectónicas, lo que esencialmente significa que puede modelar un solo concepto pero usar diferentes columnas
+dependiendo de la solicitud.
+
+Entonces, en este caso, estamos usando una arquitectura de red móvil, que es un modelo más pequeño diseñado para
+dispositivos móviles.
+
+# Descargar archivos del repositorio oficial de TensorFlow, con numerosos modelos disponibles
+https://github.com/tensorflow/models/blob/master/research/object_detection/g3doc/tf2_detection_zoo.md
+
+**The cell given below downloads a mobilenet model**
+## Download mobilenet model file
+The code below will run on Linux / MacOS systems.
+Please download the file http://download.tensorflow.org/models/object_detection/ssd_mobilenet_v2_coco_2018_03_29.tar.gz
+
+Uncompress it and put it in models folder.
+'''
+import os
+import cv2
+import numpy as np
+import urllib
+import matplotlib.pyplot as plt
+
+# **** Crear un archivo de configuración a partir de un gráfico congelado
+''' NO funciona con tensorflow macosx 2.11 tf_text_graph_ssd.py por el módulo tensorflow.tools.graph_transforms
+1. Extrae los archivos
+2. Ejecute el archivo tf_text_graph_ssd.py con la entrada como ruta al archivo frozen_graph.pb y la salida como desee.
+Se ha incluido un archivo de configuración de muestra en la carpeta de modelos'''
+
+
+# frozen_inference_graph.pb, que es el archivo de pesos para el modelo.
+modelFile = "models/ssd_mobilenet_v2_coco_2018_03_29/frozen_inference_graph.pb"
+# Archivo de configuración para la red
+configFile = "models/ssd_mobilenet_v2_coco_2018_03_29.pbtxt"
+# etiquetas de clase para el conjunto de datos que se usó para entrenar este modelo
+classFile = "coco_class_labels.txt"
+
+if not os.path.isdir('models'):
+    os.mkdir("models")
+
+if not os.path.isfile(modelFile):
+    os.chdir("models")
+    # Download the tensorflow Model
+    urllib.request.urlretrieve('http://download.tensorflow.org/models/object_detection/ssd_mobilenet_v2_coco_2018_03_29.tar.gz', 'ssd_mobilenet_v2_coco_2018_03_29.tar.gz')
+
+    # Se descomprime el fichero
+
+    # Se borra el comprimido
+    os.remove('ssd_mobilenet_v2_coco_2018_03_29.tar.gz')
+
+    # volvemos al directorio anterior
+    os.chdir("../../../OpenCV")
+
+'''Hay una gran diferencia entre un detector de objetos de aprendizaje profundo y un objeto de visión artificial 
+tradicional ( los revisados hasta ahora) Solíamos tener un detector para cada clase, por ejemplo, teníamos un detector 
+de rostros, un detector de personas y así sucesivamente, todos modelos separados. Pero con los modelos de aprendizaje 
+profundo, tenemos una enorme capacidad para aprender. Por lo tanto, un solo modelo puede detectar múltiples objetos en 
+una amplia gama de ángulos de aspecto y escalas, lo que es la verdadera belleza del aprendizaje profundo'''
+# ***** Comprobar las etiquetas de la clase
+with open(classFile) as fp:
+    labels = fp.read().split("\n")
+print(labels)
+
+
+# *****  leer el modelo de Tensorflow
+# Toma como entrada, un archivo de modelo y el archivo de configuración y nos devolverá una instancia de la red
+net = cv2.dnn.readNetFromTensorflow(modelFile, configFile)
+
+
+# ***** Detectar Objetos
+# Definimos una función para detectar archivos, Para cada archivo en el directorio
+def detect_objects(net, im):  # toma como entrada la instancia de la red y la imagen
+    dim = 300
+
+    # Crea un blob a partir de la imagen,
+    '''cuando preparamos una imagen para la inferencia, necesitamos realizar cualquier preprocesamiento en esa archivo 
+    que se realizó en el conjunto de entrenamiento. Esta función contiene varios argumentos relacionados con el 
+    preprocesamiento requerido.
+    - La imagen, 
+    - Factor de escala, establecido en uno que indica que el conjunto de entrenamiento no se le realizó ninguna 
+      escala especial.
+    - tamaño de las imágenes de entrenamiento, (dim=300) por lo que la imagen de prueba, 
+      deberán ser remodelados de acuerdo con este tamaño.
+    - valor medio, Si a las imágenes de entrenamiento se les hubiera aplicado un valor medio sustraído, entonces esto 
+     habría sido otro vector, estas imágenes no requieren ninguna resta de medios, simplemente estamos indicando 0.
+    - swapRB por si queremos o no cambiar  loa canales de colores rojo y azul. EN este ejemplo queremos hacer eso, ya 
+      que las imágenes de entrenamiento usan una convención diferente que lo que usa OpenCV.
+    - Flag de recorte, que se establece como predeterminada, es decir, las imágenes simplemente cambiarán de tamaño en 
+    lugar de recortarlas a la derecha.
+    Esta función nos devuelve una representación de blob de esa imagen que ha sido preprocesada, con lo que hay un paso 
+    de procesamiento previo, y luego también hay un paso de conversión de formato'''
+    blob = cv2.dnn.blobFromImage(im, 1.0, size=(dim, dim), mean=(0, 0, 0), swapRB=True, crop=False)
+
+    # pasa el blob a la red neuronal como entrada
+    net.setInput(blob)
+
+    # realiza la predicción, se realiza la inferencia en la imagen mediante el método net.forward()
+    objects = net.forward()
+    return objects
+
+
+def display_text(im, text, x, y):  # toma le fotograma, el texto y coordenadas
+    '''anotará un cuadro delimitador con la etiqueta de clase dibujando un rectángulo negro y lo  mete en el fotograma
+    con algún texto que indique la etiqueta de clase dentro del negro'''
+    # Obtener el tamaño del texto
+    textSize = cv2.getTextSize(text, FONTFACE, FONT_SCALE, THICKNESS)
+    dim = textSize[0]
+    baseline = textSize[1]
+
+    # Usa el tamaño del texto para crear un rectángulo negro
+    cv2.rectangle(im, (x, y - dim[1] - baseline), (x + dim[0], y + baseline), (0, 0, 0), cv2.FILLED);
+    # Display text inside the rectangle
+    cv2.putText(im, text, (x, y - 5), FONTFACE, FONT_SCALE, (0, 255, 255), THICKNESS, cv2.LINE_AA)
+
+
+# **** Mostrar Objetos
+# configuración del texto
+FONTFACE = cv2.FONT_HERSHEY_SIMPLEX
+FONT_SCALE = 0.7
+THICKNESS = 1
+
+
+# toma el fotograma, una lista de objetos detectados y el umbral de detección
+def display_objects(im, objects, threshold=0.25):
+    rows = im.shape[0];
+    cols = im.shape[1]
+
+    # Para cada objeto detectado
+    for i in range(objects.shape[2]):
+        # Encuentra la clase y la confianza
+        classId = int(objects[0, 0, i, 1])  # recupera su ID de clase
+        score = float(objects[0, 0, i, 2])  # y sus puntuaciones
+
+        # Recuperar las coordenadas originales de las coordenadas normalizadas para el cuadro delimitador
+        x = int(objects[0, 0, i, 3] * cols)
+        y = int(objects[0, 0, i, 4] * rows)
+        w = int(objects[0, 0, i, 5] * cols - x)
+        h = int(objects[0, 0, i, 6] * rows - y)
+
+        # Comprueba si la detección es de buena calidad
+        if score > threshold:
+            display_text(im, "{}".format(labels[classId]), x, y)  # llama a la función arriba definida
+            cv2.rectangle(im, (x, y), (x + w, y + h), (255, 255, 255), 2)  # introduce en la imagen un rectángulo blanco
+
+    # Convertir imagen a RGB, ya que estamos usando Matplotlib para mostrar la imagen
+    mp_img = cv2.cvtColor(im, cv2.COLOR_BGR2RGB)
+    plt.figure(figsize=(30, 10));
+    plt.imshow(mp_img);
+    plt.show();
+
+# **** Resultados
+'''estamos leyendo en una imagen de prueba, y ahora vamos a usar la función que creamos para detectar los objetos que 
+pasan en la instancia de red y la imagen leída, retornando la lista de objetos detectados'''
+im = cv2.imread('images/street.jpg')
+# llamamos a la función de visualización de objetos pasando la imagen de prueba y la matriz de objetos.
+objects = detect_objects(net, im)
+# este es un algoritmo de detección de objetos muy robusto que tiene alrededor de 80 clases.
+display_objects(im, objects)
+
+# im = cv2.imread('images/baseball.jpg')
+# objects = detect_objects(net, im)
+# display_objects(im, objects, 0.2)
+#
+# im = cv2.imread('images/soccer.jpg')
+# objects = detect_objects(net, im)
+# display_objects(im, objects)
---
+++ Training/2 DeepLearning/49. Object Detection - Chess Pieces Detection - YOLOv3 PyTorch.py
@@ -0,0 +1,447 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# ![](https://github.com/rajeevratan84/ModernComputerVision/raw/main/logo_MCV_W.png)
+# 
+# 
+# NOTE: for the most up to date version of this notebook, please copy from
+# 
+# [![Open In Colab](https://colab.research.google.com/assets/colab-badge.svg)](https://colab.research.google.com/drive/1ntAL_zI68xfvZ4uCSAF6XT27g0U4mZbW#scrollTo=VHS_o3KGIyXm)
+# 
+# 
+# 
+# 
+# ## **Training YOLOv3 object detection on a custom dataset**
+# 
+# PyTorch Edition! Thanks to [Ultralytics](https://github.com/ultralytics/yolov3/wiki/Train-Custom-Data) for making this simpler.
+# 
+# ### **Overview**
+# 
+# This notebook walks through how to train a YOLOv3 object detection model on your own dataset using Roboflow and Colab.
+# 
+# In this specific example, we'll training an object detection model to recognize chess pieces in images. **To adapt this example to your own dataset, you only need to change one line of code in this notebook.**
+# 
+# ![Chess Example](https://i.imgur.com/nkjobw1.png)
+# 
+# ### **Our Data**
+# 
+# Our dataset of 289 chess images (and 2894 annotations!) is hosted publicly on Roboflow [here](https://public.roboflow.ai/object-detection/chess-full).
+# 
+# ### **Our Model**
+# 
+# We'll be training a YOLOv3 (You Only Look Once) model. This specific model is a one-shot learner, meaning each image only passes through the network once to make a prediction, which allows the architecture to be very performant, viewing up to 60 frames per second in predicting against video feeds.
+# 
+# The GitHub repo containing the majority of the code we'll use is available [here](https://github.com/roboflow-ai/yolov3).
+# 
+# ### **Training**
+# 
+# Google Colab provides free GPU resources. Click "Runtime" → "Change runtime type" → Hardware Accelerator dropdown to "GPU."
+# 
+# Colab does have memory limitations, and notebooks must be open in your browser to run. Sessions automatically clear themselves after 12 hours.
+# 
+# ### **Inference**
+# 
+# We'll leverage the `detect.py --weights weights/last.pt` script to produce predictions. Arguments are specified below.
+# 
+# ### **About**
+# 
+# [Roboflow](https://roboflow.ai) makes managing, preprocessing, augmenting, and versioning datasets for computer vision seamless.
+# 
+# Developers reduce 50% of their boilerplate code when using Roboflow's workflow, save training time, and increase model reproducibility.
+# 
+# #### ![Roboflow Workmark](https://i.imgur.com/WHFqYSJ.png)
+# 
+# 
+# 
+# 
+# 
+# 
+# 
+
+# In[1]:
+
+
+import os
+import torch
+from IPython.display import Image, clear_output 
+print('PyTorch %s %s' % (torch.__version__, torch.cuda.get_device_properties(0) if torch.cuda.is_available() else 'CPU'))
+
+
+# In[2]:
+
+
+get_ipython().system('git clone https://github.com/roboflow-ai/yolov3  # clone')
+
+
+# ## Get Data from Roboflow
+# 
+# Create an export from Roboflow. **Select "YOLO Darknet" as the export type.**
+# 
+# Our labels will be formatted to our model's architecture.
+
+# In[7]:
+
+
+get_ipython().system('wget https://moderncomputervision.s3.eu-west-2.amazonaws.com/Chess+Pieces.v24-416x416_aug.darknet.zip')
+get_ipython().system("unzip -q 'Chess+Pieces.v24-416x416_aug.darknet.zip'")
+
+
+# ## Organize data and labels for Ultralytics YOLOv3 Implementation
+# 
+# Ultalytics's implemention of YOLOv3 calls for [a specific file management](https://github.com/ultralytics/yolov3/wiki/Train-Custom-Data) where our images are in a folder called `images` and corresponding labels in a folder called `labels`. The image and label names must match identically. Fortunately, our files are named appropriately from Roboflow.
+# 
+# We need to reorganize the folder structure slightly.
+
+# In[4]:
+
+
+get_ipython().run_line_magic('cd', 'train')
+
+
+# In[5]:
+
+
+get_ipython().run_line_magic('ls', '')
+
+
+# In[ ]:
+
+
+get_ipython().run_line_magic('mkdir', 'labels')
+get_ipython().run_line_magic('mkdir', 'images')
+
+
+# In[ ]:
+
+
+get_ipython().run_line_magic('mv', '*.jpg ./images/')
+get_ipython().run_line_magic('mv', '*.txt ./labels/')
+
+
+# In[ ]:
+
+
+get_ipython().run_line_magic('cd', 'images')
+
+
+# In[ ]:
+
+
+# create Ultralytics specific text file of training images
+file = open("train_images_roboflow.txt", "w") 
+for root, dirs, files in os.walk(""):
+    for filename in files:
+      # print("../train/images/" + filename)
+      if filename == "train_images_roboflow.txt":
+        pass
+      else:
+        file.write("../train/images/" + filename + "\n")
+file.close()
+
+
+# In[ ]:
+
+
+get_ipython().run_line_magic('cat', 'train_images_roboflow.txt')
+
+
+# In[ ]:
+
+
+get_ipython().run_line_magic('cd', '../../valid')
+
+
+# In[ ]:
+
+
+get_ipython().run_line_magic('mkdir', 'labels')
+get_ipython().run_line_magic('mkdir', 'images')
+
+
+# In[ ]:
+
+
+get_ipython().run_line_magic('mv', '*.jpg ./images/')
+get_ipython().run_line_magic('mv', '*.txt ./labels/')
+
+
+# In[ ]:
+
+
+get_ipython().run_line_magic('cd', 'images')
+
+
+# In[ ]:
+
+
+# create Ultralytics specific text file of validation images
+file = open("valid_images_roboflow.txt", "w") 
+for root, dirs, files in os.walk(""):
+    for filename in files:
+      # print("../train/images/" + filename)
+      if filename == "valid_images_roboflow.txt":
+        pass
+      else:
+        file.write("../valid/images/" + filename + "\n")
+file.close()
+
+
+# In[ ]:
+
+
+get_ipython().run_line_magic('cat', 'valid_images_roboflow.txt')
+
+
+# ## Set up model config
+# 
+# We should configure our model for training.
+# 
+# This requires editing the `roboflow.data` file, which tells our model where to find our data, our numbers of classes, and our class label names.
+# 
+# Our paths for our labels and images are correct.
+# 
+# But we need to update our class names. That's handled below..
+# 
+# 
+# 
+
+# In[ ]:
+
+
+get_ipython().run_line_magic('cd', '../../yolov3/data')
+
+
+# In[ ]:
+
+
+# display class labels imported from Roboflow
+get_ipython().run_line_magic('cat', '../../train/_darknet.labels')
+
+
+# In[ ]:
+
+
+# convert .labels to .names for Ultralytics specification
+get_ipython().run_line_magic('cat', '../../train/_darknet.labels > ../../train/roboflow_data.names')
+
+
+# In[ ]:
+
+
+def get_num_classes(labels_file_path):
+    classes = 0
+    with open(labels_file_path, 'r') as f:
+      for line in f:
+        classes += 1
+    return classes
+
+
+# In[ ]:
+
+
+# update the roboflow.data file with correct number of classes
+import re
+
+num_classes = get_num_classes("../../train/_darknet.labels")
+with open("roboflow.data") as f:
+    s = f.read()
+with open("roboflow.data", 'w') as f:
+    
+    # Set number of classes num_classes.
+    s = re.sub('classes=[0-9]+',
+               'classes={}'.format(num_classes), s)
+    f.write(s)
+
+
+# In[ ]:
+
+
+# display updated number of classes
+get_ipython().run_line_magic('cat', 'roboflow.data')
+
+
+# ## Training our model
+# 
+# Once we have our data prepped, we'll train our model using the train script.
+# 
+# By default, this script trains for 300 epochs.
+
+# In[ ]:
+
+
+get_ipython().run_line_magic('cd', '../')
+
+
+# In[ ]:
+
+
+get_ipython().system('python3 train.py --data data/roboflow.data --epochs 300')
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
+
+# ## Display training performance
+# 
+# We'll use a default provided script to display image results. **For example:**
+# 
+# ![example results](https://user-images.githubusercontent.com/26833433/63258271-fe9d5300-c27b-11e9-9a15-95038daf4438.png)
+
+# In[ ]:
+
+
+from utils import utils; utils.plot_results()
+
+
+# ## Conduct inference and display results
+# 
+# 
+
+# ### Conduct inference
+# 
+# The below script has a few key arguments we're using:
+# - **Weights**: we're specifying the weights to use for our model should be those that we most recently used in training
+# - **Source**: we're specifying the source images we want to use for our predictions
+# - **Names**: we're defining the names we want to use. Here, we're referencing `roboflow_data.names`, which we created from our Roboflow `_darknet.labels`italicized text above.
+
+# In[ ]:
+
+
+get_ipython().system('python3 detect.py --weights weights/last.pt --source=../test --names=../train/roboflow_data.names')
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
+
+# ### Displaying our results
+# 
+# Ultralytics generates predictions which include the labels and bounding boxes "printed" directly on top of our images. They're saved in our `output` directory within the YOLOv3 repo we cloned above.
+
+# In[ ]:
+
+
+# import libraries for display
+get_ipython().run_line_magic('matplotlib', 'inline')
+import matplotlib.pyplot as plt
+import numpy as np
+from IPython.display import display, Image
+from glob import glob
+import random
+import PIL
+
+
+# In[ ]:
+
+
+# plot just one random image prediction
+filename = random.choice(os.listdir('./output'))
+print(filename)
+Image('./output/' + filename)
+
+
+# In[ ]:
+
+
+# grab all images from our output directory
+images = [ PIL.Image.open(f) for f in glob('./output/*') ]
+
+
+# In[ ]:
+
+
+# convert images to numPy
+def img2array(im):
+    if im.mode != 'RGB':
+        im = im.convert(mode='RGB')
+    return np.fromstring(im.tobytes(), dtype='uint8').reshape((im.size[1], im.size[0], 3))
+
+
+# In[ ]:
+
+
+# create array of numPy images
+np_images = [ img2array(im) for im in images ]
+
+
+# In[ ]:
+
+
+# plot ALL results in test directory (NOTE: adjust figsize as you please)
+for img in np_images:
+    plt.figure(figsize=(8, 6))
+    plt.imshow(img)
+
+
+# In[ ]:
+
+
+
+
+
+# ## Save Our Weights
+# 
+# We can save the weights of our model to use them for inference in the future, or pick up training where we left off. 
+# 
+# We can first save them locally. We'll connect our Google Drive, and save them there.
+# 
+
+# In[ ]:
+
+
+# save locally
+from google.colab import files
+files.download('./weights/last.pt')
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+# connect Google Drive
+from google.colab import drive
+drive.mount('/content/gdrive')
+
+
+# In[ ]:
+
+
+get_ipython().run_line_magic('pwd', '')
+
+
+# In[ ]:
+
+
+# create a copy of the weights file with a datetime 
+# and move that file to your own Drive
+get_ipython().run_line_magic('cp', './weights/last.pt ./weights/last_copy.pt')
+get_ipython().run_line_magic('mv', './weights/last_copy.pt /content/gdrive/My\\ Drive')
+
+
+# In[ ]:
+
+
+
+
