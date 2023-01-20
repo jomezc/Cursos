5a4e19dae1a3129001cd58b75744e27741861155
jomezc <73670380+jomezc@users.noreply.github.com>
2023-01-20 18:00:49 +0100

    Curso

--- Training/1 OpenCV/12 Momentos, Clasificación, Aproximación y Correspondencia de Contornos.py
+++ Training/1 OpenCV/12 Momentos, Clasificación, Aproximación y Correspondencia de Contornos.py
@@ -19,7 +19,7 @@ from matplotlib import pyplot as plt
 def imshow(title = "Image", image = None, size = 16):
     w, h = image.shape[0], image.shape[1]
     aspect_ratio = w/h
-    plt.figure(figsize=(size * aspect_ratio,size))
+    plt.figure(figsize=(size * aspect_ratio, size))
     plt.imshow(cv2.cvtColor(image, cv2.COLOR_BGR2RGB))
     plt.title(title)
     plt.show()
